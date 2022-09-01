9c6b656142a021cfcdfc596bf96849cf4062a514
LopezYeshua <lopezyeshua@gmail.com>
2022-08-31 22:34:44 -0700

    added css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Main from './views/Main';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Main />
     </div>
   );
--- client/src/components/ProductForm.jsx
+++ client/src/components/ProductForm.jsx
@@ -1,23 +1,38 @@
+import '../App.css';
 import React, { useState } from 'react'
 import axios from 'axios';
+
+// Start of anonymous function
 export default () => {
     const [title, setTitle] = useState("");
     const [price, setPrice] = useState(0);
     const [description, setDescription] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
 
+    // Sends to api
     const onSubmitHandler = e => {
-        e.preventDefault();
+        e.preventDefault(); // prevents screen refresh
         axios.post('http://localhost:8000/api/products', {
             title,
             price,
             description
         })
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
-    }
+        .then(res => console.log(res)) // on success
+        .catch(err => {
+            console.log(err);
+            setSuccessMsg("");
+        }) // on fail
 
+        setTitle('');
+        setDescription('');
+        setPrice(0);
+        setSuccessMsg("Success!")
+    }
+//  anonymous function returns form
     return (
         <form onSubmit={ onSubmitHandler }>
+            <h1>Add a Product</h1>
+            <p className={successMsg != "" ? "success-msg":"" }>{successMsg}</p>
             <div>
                 <label>Title</label>
                 <input type="text" onChange={(e) => setTitle(e.target.value)} value={ title }/>
@@ -27,7 +42,7 @@ export default () => {
                 <input type="number" onChange={(e) => setPrice(e.target.value)} value={ price }/>
             </div>
             <div>
-                <label>Title</label>
+                <label>Description</label>
                 <input type="text" onChange={(e) => setDescription(e.target.value)} value={ description }/>
             </div>
             <input type="submit" value='SUBMIT' />
